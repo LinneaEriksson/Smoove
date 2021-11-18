c9a7ac107ee781897aa720b69b140bbc130efee6
LinneaEriksson <Linnea-E@hotmail.com>
2021-11-18 22:03:43 +0100

    test js

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 function myFunction() {
-    const x = document.getElementById("myLinks");
-    if (x.style.display === "block") {
-      x.style.display = "none";
-    } else {
-      x.style.display = "block";
-    }
+  const x = document.getElementById("myLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
   }
+}
+
+const gridItem = document.querySelector(".HoverPicture");
+
+const imageOfMe = gridItem.querySelector(".HoverPicture img");
+imageOfMe.src = "images/testbild.jpg";
