145310e482e1680157a0227128f83fe732138add
NellySP <nelly.petren@gmail.com>
2021-11-30 14:11:45 +0100

    kommentar

--- index.js
+++ index.js
@@ -53,7 +53,7 @@ function currentSlide(n) {
 function showSlides(n) {
   var i;
   var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("dot");
+  // var dots = document.getElementsByClassName("dot");
   if (n > slides.length) {
     slideIndex = 1;
   }
@@ -63,15 +63,14 @@ function showSlides(n) {
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
+  // for (i = 0; i < dots.length; i++) {
+  //   dots[i].className = dots[i].className.replace(" active", "");
+  // }
   slides[slideIndex - 1].style.display = "block";
-  dots[slideIndex - 1].className += " active";
 }
 
 // year
 
-const d = new Date();
-let year = d.getFullYear();
-document.getElementById("demo").innerHTML = year;
+// const d = new Date();
+// let year = d.getFullYear();
+// document.getElementById("demo").innerHTML = year;
