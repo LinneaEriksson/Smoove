331fa1bf85e1970d17d130f61caa097df8cdead9
NellySP <nelly.petren@gmail.com>
2021-12-02 09:23:42 +0100

    Update index.js

--- index.js
+++ index.js
@@ -11,6 +11,12 @@ function myFunction() {
   }
 }
 
+// forn
+
+function form() {
+  document.getElementById("myForm").reset();
+}
+
 // Functions to change the color of the car. MOBILE-VERSION
 
 function PictureChangeBlack() {
